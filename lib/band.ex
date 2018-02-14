71b5e70fb97ab07cbeed17f94305ca7488b875cc
Sonny Scroggin <sonny@scrogg.in>
2018-02-14 14:10:39 -0600

    Allow runtime configuration
    Signed-off-by: Sonny Scroggin <sonny@scrogg.in>

--- lib/band.ex
+++ lib/band.ex
@@ -1,3 +1,28 @@
 defmodule Band do
-  use Statix
+  use Statix, runtime_config: true
+  use GenServer
+
+  def child_spec(config) do
+    %{id: __MODULE__,
+      start: {__MODULE__, :start_link, [config]},
+      restart: :temporary,
+      type: :worker,
+      shutdown: 5000}
+  end
+
+  def start_link(config) do
+    GenServer.start_link(__MODULE__, config)
+  end
+
+  def init(config) do
+    :band
+    |> Application.get_all_env()
+    |> Keyword.take([:prefix, :host, :port])
+    |> Keyword.merge(config)
+    |> Enum.each(fn {key, value} -> Application.put_env(:statix, key, value) end)
+
+    Band.connect()
+
+    :ignore
+  end
 end
--- test/band_test.exs
+++ test/band_test.exs
@@ -1,4 +1,41 @@
 defmodule BandTest do
   use ExUnit.Case
   doctest Band
+
+  test "application env config" do
+    Application.put_env(:band, :prefix, "default")
+    Application.put_env(:band, :host, "localhost")
+    Application.put_env(:band, :port, 8125)
+
+    Band.start_link([])
+
+    statix = Application.get_all_env(:statix)
+    assert statix[:prefix] == "default"
+    assert statix[:host] == "localhost"
+    assert statix[:port] == 8125
+  end
+
+  test "runtime config overrides application env" do
+    Application.put_env(:band, :prefix, "default")
+    Application.put_env(:band, :host, "statsd")
+    Application.put_env(:band, :port, 8125)
+
+    Band.start_link([prefix: "changed", host: "localhost", port: 1234])
+
+    statix = Application.get_all_env(:statix)
+
+    assert statix[:prefix] == "changed"
+    assert statix[:host] == "localhost"
+    assert statix[:port] == 1234
+  end
+
+  test "child_spec/1" do
+    assert Band.child_spec([]) == %{
+      id: Band,
+      start: {Band, :start_link, [[]]},
+      restart: :temporary,
+      type: :worker,
+      shutdown: 5000
+    }
+  end
 end
